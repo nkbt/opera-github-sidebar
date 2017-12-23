255895aa7cd65efca7085d79206a8cbb451c0f64
Nik Butenko <nik@butenko.me>
2017-12-24 09:28:20 +1100

    Detect Github URLs and only redraw if we are on one of them

--- sidebar.js
+++ sidebar.js
@@ -1,4 +1,42 @@
 const e = (...args) => document.querySelector(...args);
 
-window.addEventListener('load', () =>
-  e('[data-app]').innerHTML = 'Loaded', false);
+
+const options = {
+};
+
+
+const render = url => {
+  e('[data-app]').innerHTML = url;
+};
+
+
+const maybeRender = () => {
+  if (!Object.keys(options).length) {
+    return;
+  }
+  chrome.tabs.query({
+    'currentWindow': true,
+    'active': true,
+    'url': 'https://github.com/*'
+  }, ([{url} = {}]) => url && render(url));
+};
+
+
+const restoreSettings = () =>
+  chrome.storage.sync.get(null, opts => {
+    Object.assign(options, opts);
+    maybeRender();
+  });
+
+
+const updateSettings = changes => {
+  Object.keys(changes).forEach(key => Object.assign(options, {[key]: changes[key].newValue}));
+  maybeRender();
+};
+
+
+window.addEventListener('load', restoreSettings, false);
+chrome.storage.onChanged.addListener(updateSettings);
+
+chrome.tabs.onUpdated.addListener(maybeRender);
+chrome.tabs.onActivated.addListener(maybeRender);
